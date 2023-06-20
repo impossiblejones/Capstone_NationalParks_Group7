dd5b8138c3578403d6e8468e3b1a45f21d4f45ad
Jeremy-Goodway_lmig <jeremy.goodway@libertymutual.com>
2023-06-20 08:42:11 -0700

    NavBar Shell Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import HomePage from './HomePage';
+import NavBar from './NavBar.js';
 
 function App() {
   return (
     <div className="App">
+			
+				<NavBar/>
+			
         <HomePage/>
 
 
---
+++ src/NavBar.js
@@ -0,0 +1,26 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+	return (
+		<header className='NavBar'>
+			<h2 className='test'>Proof Is In The Pudding</h2>
+
+			<ul>
+				<li>
+					<p>LOGO HERE</p>
+				</li>
+				<li>
+					<p>Home</p>
+				</li>
+				<li>
+					<p>Mountains</p>
+				</li>
+				<li>
+					<p>National Parks</p>
+				</li>
+			</ul>
+		</header>
+	);
+}
+
+export default NavBar
