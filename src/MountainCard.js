7e1d0508a407be080a42567b5a841df7d1aa7f08
Jeremy-Goodway_lmig <jeremy.goodway@libertymutual.com>
2023-06-21 12:41:24 -0700

    MountainCard Styled

--- src/MountainCard.js
+++ src/MountainCard.js
@@ -1,27 +1,34 @@
-
+import './MountainCard.css'
 
 const MountainTest = {
-    "name": "Mt. Washington",
-    "elevation": 6288,
-    "effort": "Strenuous",
-    "img": "Washington-StoryImage_2.jpg",
-    "desc": "Mt. Washington (6,288 feet) is the highest peak east of the Mississippi River and north of the Carolinas. The upper part of the mountain has a climate similar to that of northern Labrador and supports a variety of alpine flora and fauna.",
-    "coords": {
-        "lat": 44.270403,
-        "lng": -71.303501
-    }
+	"name": "Mt. Washington",
+	"elevation": 6288,
+	"effort": "Strenuous",
+	"img": "Washington-StoryImage_2.jpg",
+	"desc": "Mt. Washington (6,288 feet) is the highest peak east of the Mississippi River and north of the Carolinas. The upper part of the mountain has a climate similar to that of northern Labrador and supports a variety of alpine flora and fauna.",
+	"coords": {
+		"lat": 44.270403,
+		"lng": -71.303501
+	}
 }
+
+
 const MountainCard = () => {
-    return (
-        <div className="MountainCard">   
-            <h2>name</h2>
-            <p>elevation</p>
-            <p>effort</p>
-            <img/>
-            <p>desc</p>
-            <p>coords</p>
-        </div>
-);
+	return (
+		<div className="MountainCard">
+			<div>
+				<img src='./images/mountains/Washington-StoryImage_2.jpg' />
+				<h2>{MountainTest.name}</h2>
+			</div>
+			<article className='mountainData'>
+				<p>elevation: {MountainTest.elevation} ft</p>
+				<p>effort: {MountainTest.effort}</p>
+
+				<p>Description: {MountainTest.desc}</p>
+				<p>coords ( Latitude: {MountainTest.coords.lat}, Longitude: {MountainTest.coords.lng})</p>
+			</article>
+		</div>
+	);
 }
 
 export default MountainCard;
