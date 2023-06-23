37241192703f08eb620a5e3db148ccbf15efe70c
Jeremy-Goodway_lmig <jeremy.goodway@libertymutual.com>
2023-06-23 10:02:28 -0700

    wipTblPopulate

--- src/NationalParksPage.js
+++ src/NationalParksPage.js
@@ -1,6 +1,8 @@
 import './NationalParksPage.css';
 import { useState, useEffect } from 'react';
 
+console.clear();  //reset the log on load
+
 const NationalParksPage = () => {
 
 	const [stateList, setStateList] = useState([]);
@@ -19,11 +21,11 @@ const NationalParksPage = () => {
 			.then(response => response.parks)
 			.then(response => setNationalsParksData(response))
 	}
+
 	useEffect(() => {
 		getListData('./data/parktypes.json', setParkTypeList)
 		getListData('./data/locations.json', setStateList)
 		getNationalParksData('./data/nationalparks.json')
-
 	}, [])
 
 
@@ -32,6 +34,40 @@ const NationalParksPage = () => {
 	}
 
 	// nationalParksData[0] && console.log(Object.keys(nationalParksData[0]))
+
+	// const nationalParkRows = () => {
+	// 	let parkData = [];
+
+	// 	for (let i = 0; i < 10; i++) {
+	// 		// console.log(nationalParksData[i])
+	// 		parkData.push(Object.values(nationalParksData[i]).slice(0, -1)  )
+	// 		// console.log('parkData')
+	// 		// console.log(parkData)
+	// 	}
+	// 	return parkData;
+	// }
+	// LocationID
+	// LocationName
+	// Address
+	// City
+	// State
+
+	// {
+	//   "LocationID": "ABLI",
+	//   "LocationName": "Abraham Lincoln Birthplace National Historical Park",
+	//   "Address": "2995 Lincoln Farm Road",
+	//   "City": "Hodgenville",
+	//   "State": "Kentucky",
+	//   "ZipCode": 42748,
+	//   "Phone": "(270) 358-3137",
+	//   "Fax": "(270) 358-3874",
+	//   "Latitude": 37.535671,
+	//   "Longitude": -85.7340637,
+	//   "Location":{"coordinates": [-85.7340637,37.535671],"type":"Point"}
+	// }
+
+
+
 
 	return (
 		<>
@@ -51,20 +87,37 @@ const NationalParksPage = () => {
 			<table>
 				<thead>
 					<tr>
-						{nationalParksData[0] && Object.keys(nationalParksData[0]).map(header => {
+						{(nationalParksData && nationalParksData.length) && Object.keys(nationalParksData[0]).slice(0,5).map(header => {
 							return <th key={header} value={header}>{header}</th>
 						})}
 					</tr>
 				</thead>
 				<tbody>
+					{(nationalParksData && nationalParksData.length) && nationalParksData.map(parkDim => {
+						return (
+							<tr key={parkDim.locationID}>
+								<td>{parkDim.LocationID}</td>
+								<td>{parkDim.LocationName}</td>
+								<td>{parkDim.Address}</td>
+								<td>{parkDim.City}</td>
+								<td>{parkDim.State}</td>
+
+							</tr>
+						)
+						// return (
+						// 	<tr key={parkDim[0]}>
+						// 		{parkDim[0] && parkDim.map(dim => {
+						// 			// return <td>test</td>
+						// 			return (<td value={dim}>{dim}</td>)
+						// 		})}
+						// 	</tr>
+						// )
+					})}
 
 				</tbody>
 
 
 			</table>
-
-
-
 		</>
 	);
 }
