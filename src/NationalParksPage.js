cd1a37843535e6be18f806c4e84987911354dd31
impossiblejones <127601578+impossiblejones@users.noreply.github.com>
2023-06-23 12:43:28 -0500

    FilteronSubmit

--- src/NationalParksPage.js
+++ src/NationalParksPage.js
@@ -1,13 +1,17 @@
 import './NationalParksPage.css';
 import { useState, useEffect } from 'react';
 
-console.clear();  //reset the log on load
+
 
 const NationalParksPage = () => {
 
 	const [stateList, setStateList] = useState([]);
 	const [parkTypeList, setParkTypeList] = useState([]);
 	const [nationalParksData, setNationalsParksData] = useState({})
+	const [filterValues, setFilterValues] = useState({})
+
+
+
 
 	const getListData = (path, listSetFunc) => {
 		fetch(path)
@@ -28,45 +32,23 @@ const NationalParksPage = () => {
 		getNationalParksData('./data/nationalparks.json')
 	}, [])
 
+	const changeFilterHandler = event => {
+		const { name, value } = event.target
+		setFilterValues({ ...filterValues, [name]: value })
+	}
+
 
 	const submitParksFilterHandler = event => {
 		event.preventDefault()
+		const filterData = nationalParksData.filter(park => {
+			return park.StateList === filterValues.stateList
+				&& park.LocationName.includes(filterValues.TypeList)
+
+		})
+		setNationalsParksData(filterData)
+
+
 	}
-
-	// nationalParksData[0] && console.log(Object.keys(nationalParksData[0]))
-
-	// const nationalParkRows = () => {
-	// 	let parkData = [];
-
-	// 	for (let i = 0; i < 10; i++) {
-	// 		// console.log(nationalParksData[i])
-	// 		parkData.push(Object.values(nationalParksData[i]).slice(0, -1)  )
-	// 		// console.log('parkData')
-	// 		// console.log(parkData)
-	// 	}
-	// 	return parkData;
-	// }
-	// LocationID
-	// LocationName
-	// Address
-	// City
-	// State
-
-	// {
-	//   "LocationID": "ABLI",
-	//   "LocationName": "Abraham Lincoln Birthplace National Historical Park",
-	//   "Address": "2995 Lincoln Farm Road",
-	//   "City": "Hodgenville",
-	//   "State": "Kentucky",
-	//   "ZipCode": 42748,
-	//   "Phone": "(270) 358-3137",
-	//   "Fax": "(270) 358-3874",
-	//   "Latitude": 37.535671,
-	//   "Longitude": -85.7340637,
-	//   "Location":{"coordinates": [-85.7340637,37.535671],"type":"Point"}
-	// }
-
-
 
 
 	return (
@@ -74,12 +56,12 @@ const NationalParksPage = () => {
 			<h2>National Parks</h2>
 			<form onSubmit={submitParksFilterHandler}>
 				<label htmlFor="StateList">State</label>
-				<select name='StateList' id='StateList'>
+				<select name='StateList' id='StateList' onChange={changeFilterHandler}>
 					{stateList.map(state => <option key={state} value={state}>{state}</option>)}
 				</select>
 
 				<label htmlFor="TypeList">Type</label>
-				<select name='TypeList' id='TypeList'>
+				<select name='TypeList' id='TypeList' onChange={changeFilterHandler}>
 					{parkTypeList.map(type => <option key={type} value={type}>{type}</option>)}
 				</select>
 				<input type='submit' />
@@ -87,7 +69,7 @@ const NationalParksPage = () => {
 			<table>
 				<thead>
 					<tr>
-						{(nationalParksData && nationalParksData.length) && Object.keys(nationalParksData[0]).slice(0,5).map(header => {
+						{(nationalParksData && nationalParksData.length) && Object.keys(nationalParksData[0]).slice(0, 5).map(header => {
 							return <th key={header} value={header}>{header}</th>
 						})}
 					</tr>
@@ -104,14 +86,7 @@ const NationalParksPage = () => {
 
 							</tr>
 						)
-						// return (
-						// 	<tr key={parkDim[0]}>
-						// 		{parkDim[0] && parkDim.map(dim => {
-						// 			// return <td>test</td>
-						// 			return (<td value={dim}>{dim}</td>)
-						// 		})}
-						// 	</tr>
-						// )
+
 					})}
 
 				</tbody>
